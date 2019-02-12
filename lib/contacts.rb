b78723959edb05fe524a78fd8953a43d41b601e7
Vinh Do <vinhdo07@gmail.com>
2019-02-12 03:36:06 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,11 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each_key do |name|
+    name.each do |key, value|
+      if key == favorite_ice_cream_flavors
+        value.delete("strawberry")
+      end
+    end
+  end
 end
